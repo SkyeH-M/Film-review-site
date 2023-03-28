a752c8d1d0976a02d786462ff26dc57f33de63ad
SkyeH-M <justskye98@gmail.com>
2023-03-28 16:17:10 +0000

    Begun search, and add_film functionality

--- filmreview/models.py
+++ filmreview/models.py
@@ -5,6 +5,7 @@ class Users(db.Model):
     """ Schema for Users model for login/signup """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String, unique=False, nullable=False)
 
     def __repr__(self):
         # __repr__ to rep itself in the form of a string
--- filmreview/routes.py
+++ filmreview/routes.py
@@ -5,4 +5,14 @@ from filmreview.models import Users, Watch_list
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+
+@app.route("/add_film", methods=["GET", "POST"])
+def add_film():
+    return render_template("add_film.html")
