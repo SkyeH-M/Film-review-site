8dbcd0b820f5a1fe02c4c183d4ddcd31173a4d3f
SkyeH-M <justskye98@gmail.com>
2023-03-29 11:22:56 +0000

    Initialised login.html and signup.html and their respective forms

--- filmreview/models.py
+++ filmreview/models.py
@@ -1,15 +1,18 @@
 from filmreview import db
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, BooleanField
+# from wtforms.validators import InputRequired, Email, Length
 
 
-class Users(db.Model):
-    """ Schema for Users model for login/signup """
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String, unique=False, nullable=False)
+# class Users(db.Model):
+#     """ Schema for Users model for login/signup """
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(25), unique=True, nullable=False)
+#     password = db.Column(db.String, unique=False, nullable=False)
 
-    def __repr__(self):
-        # __repr__ to rep itself in the form of a string
-        return self.username
+#     def __repr__(self):
+#         # __repr__ to rep itself in the form of a string
+#         return self.username
 
 
 class Watch_list(db.Model):
@@ -21,3 +24,12 @@ class Watch_list(db.Model):
     def __repr__(self):
         # __repr__ to rep itself in the form of a string
         return self.list_name
+
+
+# # below is from Pretty Printed flask login
+# class LoginForm(FlaskForm):
+#     username = StringField('username', validators=[InputRequired(
+#     ), Length(min=4, max=20)])
+#     password = PasswordField('password', validators=[InputRequired(
+#     ), Length(min=5, max=80)])
+#     remember = BooleanField('remember me')
--- filmreview/routes.py
+++ filmreview/routes.py
@@ -1,6 +1,29 @@
 from flask import render_template
 from filmreview import app, db
-from filmreview.models import Users, Watch_list
+from filmreview.models import Watch_list  # Users removed from here?
+# below is from Pretty Printed, User Login with Flask-Login
+# installed flaskwtf with pip install -U Flask-WTF
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+
+
+# below is from Pretty Printed flask login
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(
+    ), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(
+    ), Length(min=5, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[
+        InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(
+    ), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(
+    ), Length(min=5, max=80)])
 
 
 @app.route("/")
@@ -16,3 +39,15 @@ def search():
 @app.route("/add_film", methods=["GET", "POST"])
 def add_film():
     return render_template("add_film.html")
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
+
+@app.route("/signup")
+def signup():
+    form = RegisterForm()
+    return render_template("signup.html", form=form)
