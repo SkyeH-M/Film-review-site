723c7c3ff22778befc66375b7067781ba5e9bafe
SkyeH-M <justskye98@gmail.com>
2023-03-30 10:04:12 +0000

    Switched to Bootstrap4

--- app.py
+++ app.py
@@ -3,17 +3,27 @@
 
 from flask import Flask, render_template
 # from here this is pretty printed imports
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 
 
 app = Flask(__name__)
-Bootstrap(app)
+bootstrap = Bootstrap4(app)
 
 
 class LoginForm(FlaskForm):
+    username = StringField("username", validators=[InputRequired(
+    ), Length(min=4, max=15)])
+    password = PasswordField("password", validators=[InputRequired(
+    ), Length(min=5, max=80)])
+    remember = BooleanField("remember me")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(
+        message='Invalid email'), Length(max=50)])
     username = StringField("username", validators=[InputRequired(
     ), Length(min=4, max=15)])
     password = PasswordField("password", validators=[InputRequired(
@@ -44,6 +54,7 @@ def login():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    form = RegisterForm()
     return render_template("signup.html", form=form)
 
 
