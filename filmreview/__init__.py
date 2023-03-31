cbb4e98f985accc2ea45710fbe8f6bc7897788d1
SkyeH-M <justskye98@gmail.com>
2023-03-31 14:14:45 +0000

    Commented out section on __init__.py

--- filmreview/__init__.py
+++ filmreview/__init__.py
@@ -12,5 +12,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 
 db = SQLAlchemy(app)  # removed app from ()
+# db.drop_all()
+# db.create_all()
+
+
+# db.init_app(app)
+
+# with app.app_context():
+#     from . import routes  # Import routes
+#     db.create_all()  # Create sql tables for our data models
+
+# # return app
 
 from filmreview import routes  # noqa
