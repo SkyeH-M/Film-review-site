604808646fa964954e93974402438554669ad193
SkyeH-M <justskye98@gmail.com>
2023-04-07 12:48:51 +0000

    Added flash messages and logout functionality

--- filmreview/routes.py
+++ filmreview/routes.py
@@ -3,7 +3,7 @@
 # 1) didn't include form.hidden_tag() on login!
 
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 # installed flask-bootstrap with 'pip install flask-bootstrap'
 # from flask_bootstrap import Bootstrap
 from filmreview import app, db
@@ -15,15 +15,20 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 # is the indentation below an issue?
-# from flask_login import (LoginManager, UserMixin, login_user, login_required,
-#                          logout_user, current_user)
+from flask_login import (LoginManager, UserMixin, login_user, login_required,
+                         logout_user, current_user)
 
 # below from Pretty Printed
 bootstrap = Bootstrap4(app)
 
-# login_manager = LoginManager(app)
-# login_manager.init_app(app)
+login_manager = LoginManager(app)
+login_manager.init_app(app)
 # login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
 
 class LoginForm(FlaskForm):
@@ -31,7 +36,6 @@ class LoginForm(FlaskForm):
     ), Length(min=4, max=20)])
     password = PasswordField("password", validators=[InputRequired(
     ), Length(min=5, max=80)])
-    remember = BooleanField("remember me")
 
 
 class RegisterForm(FlaskForm):
@@ -49,13 +53,13 @@ def home():
 
 
 @app.route("/search")
-# @login_required
+@login_required
 def search():
-    return render_template("search.html")
+    return render_template("search.html", name=current_user.username)
 
 
 @app.route("/add_film", methods=["GET", "POST"])
-# @login_required
+@login_required
 def add_film():
     return render_template("add_film.html")
 
@@ -66,11 +70,15 @@ def login():
     # has form been submit? if yes continue, if no render_template
     if request.method == 'POST' and form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
-        if user:
-            # this works!
+        if user is None:
+            flash('Please register an account, or login to gain access')
+            return redirect(url_for('signup'))
+        if user is not None:
             if check_password_hash(user.password, form.password.data):
-                return redirect(url_for('home'))
-                # this also works!
+                # signs user in, can now access loginRequired pages
+                login_user(user)
+                # this redirect will be changed to films
+                return redirect(url_for('search'))
         return '<h1>Invalid username or password</h1>'
     return render_template("login.html", form=form)
 
@@ -93,6 +101,12 @@ def signup():
         #  {form.password.data} </h1>"
     return render_template("signup.html", form=form)
 
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 # delete first database record for Users as password is not yet hashed
 # Users.query.filter_by(id=1).delete()
 # db.session.commit()
